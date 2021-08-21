8f539db0b1219809bbf7d552234ef26c2a916488
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2021-08-21 14:35:49 +0530

    Take Input from user, Insert Node in LL

--- OOPS & Data Structures/src/LinkedList/LinkedListUse.java
+++ OOPS & Data Structures/src/LinkedList/LinkedListUse.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class LinkedListUse {
 	
 	public static Node<Integer> createLinkedList(){
@@ -44,6 +46,8 @@ public class LinkedListUse {
         return length;
 	}
 	
+	//ith Element
+	
 	public static int ithElement(Node<Integer> head, int i) {
 		Node<Integer> temp1 = head;
         int length = -1;
@@ -59,25 +63,103 @@ public class LinkedListUse {
         return -1;
 	}
 	
+	// print linked list
+	
 	public static void print(Node<Integer> head) {
 		Node<Integer> temp = head;
 		while(temp != null) {
-			System.out.println(temp.data);
+			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
 		
 	}
 
+	//Take input from the user
+	
+	public static Node<Integer> takeInput(){
+		
+		Scanner in = new Scanner(System.in);
+		
+		int data = in.nextInt();
+		
+		Node<Integer> head = null;
+		Node<Integer> temp = head;
+		while(data != -1) {
+			Node<Integer> currnode = new Node<>(data);
+			if(head == null) {
+				head = currnode;
+				temp = head;
+			}else {
+				temp.next = currnode;
+				temp = currnode;
+			}
+			data = in.nextInt();
+		}
+		
+		return head;
+		
+		
+	}
+	
+	//Insert Element
+	
+	public static Node<Integer> insert(Node<Integer> head, int data,int index){
+		
+		Node<Integer> currNode = new Node<>(data);
+		System.out.println();
+		
+		// If index is 0
+		
+		if(index == 0) {
+			currNode.next = head;
+			head = currNode;
+			return head;
+		}
+		
+		int count = 0;
+		Node<Integer> prev = head;
+	
+		// if we want to insert at index 3 go till index 2 and exit loop
+		
+		while(prev != null) {
+			prev = prev.next;
+			count++;
+			if(count == index-1) {
+				break;
+			}
+		}
+		
+		// If index is not present
+		if(index > count+1){
+			return head;
+        }
+		
+		// Now we are at previous index, we want to add next to it
+		// Or
+		// if we want to add at last index
+		
+		currNode.next = prev.next;
+		prev.next = currNode;
+ 		
+		return head;
+	}
+	
+	// Main Function
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Node<Integer> head = createLinkedList();
+//		Node<Integer> head = createLinkedList();
 //		increment(head);
+		Node<Integer> head = takeInput();
 		
-//		print(head);
-		System.out.println("length "+ length(head));
-		System.out.println("3rd Element "+ ithElement(head,4) );
+		print(head);
 		
+		head = insert(head,20,20);
+		print(head);
+//		System.out.println("length "+ length(head));
+//		System.out.println("3rd Element "+ ithElement(head,4) );
+//		
 
 	}
 
